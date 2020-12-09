86071b94df160ccf412a253e99db93fbde16357a
Dan Buckland <danbucklan@gmail.com>
2020-12-09 12:28:54 +0000

    Tidy up and remove unnecessary i variable

--- udemy-course/sliding-window/max-subarray-sum.js
+++ udemy-course/sliding-window/max-subarray-sum.js
@@ -1,31 +1,29 @@
 // Write a function called 'maxSubarraysum' which accepts an array of integers and a number 'n'.
-// The function should calculate the maximum sum of n consecutive elements in the array
+// The function should calculate the maximum sum of n consecutive elements in the array.
+// The complexity should be O(n)
 
 // Assumes the array could be mutated
 // Assumes the array could be unsorted
 // Empty array should return null
 // n = 0 should return null
 // Array can contain negative numbers
+
 const maxSubarraySum = (array, n) => {
   // short circuit if array is empty, n < 1 or n > array length
   if (array.length === 0 || n < 1 || n > array.length) { return null }
   // get the sum of the first n numbers in array
-  // const reducer = (accumulator, currentValue) => accumulator + currentValue
-  // let windowTotal = array.slice(0, n).reduce(reducer) -> pretty but less performant!
-  let windowTotal = 0;
+  let windowTotal = 0
   for (let i = 0; i < n; i++) {
-    windowTotal += array[i];
+    windowTotal += array[i]
   }
   // set max to initial total
   let max = windowTotal
   // loop through array until window reaches end of the array
-  let i = 0
   for (let j = n; j < array.length; j++) {
     // delete first item in the window and add the last
-    windowTotal = windowTotal - array[i] + array[j]
+    windowTotal = windowTotal - array[j - n] + array[j]
     max = Math.max(windowTotal, max)
     // slide the window along
-    i++
   }
   return max
 }
