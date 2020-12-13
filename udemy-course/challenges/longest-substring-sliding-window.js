bc3f3dd62fbdf5507b883fb1834f7b872a762454
Dan Buckland <danbucklan@gmail.com>
2020-12-13 10:22:24 +0000

    Add first working attempt and longest substring challenge

---
+++ udemy-course/challenges/longest-substring-sliding-window.js
@@ -0,0 +1,51 @@
+// Write a function called findLongestSubstring, which accepts a string and 
+// returns the length of the longest substring with all distinct characters
+
+// Assume no spaces or punctuation
+// Assume string will always be lowercase
+
+const findLongestSubstring = (string) => {
+  if (!string) { return 0 }
+  // create an object to store the character and position
+  let charPositions = {}
+  // add a left and right marker starting at 0 and 1
+  let left = 0
+  // store a count starting at 1
+  let count = 1
+  let maxLength = 1
+  // store left value with position
+  charPositions[string[left]] = 0
+
+  for (let right = 1; right < string.length; right++) {
+    // if right does not exist
+    debugger
+    if (!(string[right] in charPositions) || charPositions[string[right]] < left) { // avoids keys set to 0 returning falsey
+      // increment count
+      count++
+      // add right to object as key with position
+      charPositions[string[right]] = right
+    } else {
+      maxLength = Math.max(maxLength, count)
+      // get the value stored for the key
+      // move left to value + 1
+      left = charPositions[string[right]] + 1
+      // set the new count to the length of the current substring
+      count = right - (left - 1);
+      // set value to right
+      charPositions[string[right]] = right
+    }
+  }
+  return maxLength;
+}
+
+// Test cases
+console.log(findLongestSubstring('moon')) // 2
+console.log(findLongestSubstring('abracadabra')) // 4
+console.log(findLongestSubstring('')) // 0
+console.log(findLongestSubstring('aaaaaaaaa')) // 1
+console.log(findLongestSubstring('findlongestsubstring')) // 8
+
+
+
+// 'findlongestsubstring'
+//  --x---x
