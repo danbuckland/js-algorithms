bef6410e6fa57d4630f607ba8fde5b90d9f39f62
Dan Buckland <danbucklan@gmail.com>
2020-12-19 18:53:01 +0000

    First attempt at insertion sort using splice

---
+++ udemy-course/challenges/sorting-insertion-sort.js
@@ -0,0 +1,28 @@
+const insertionSort = (array) => {
+  let sortedEnd = 1
+  // loop through all elements starting from the end of the sorted array
+  for (let i = sortedEnd; i < array.length; i++) {
+    for (let j = 0; j < sortedEnd + 1; j++) {
+      if (j === sortedEnd) {
+        // item is in the right place
+        sortedEnd++
+        break
+      } else if (array[i] <= array[j]) {
+        // insert the item at this point
+        moveItem(array, i, j)
+        sortedEnd++
+        break
+      }
+    }
+  }
+  return array
+}
+
+const moveItem = (array, from, to) => {
+  let passenger = array.splice(from, 1) // Grab the item and remove it
+  array.splice(to, 0, ...passenger) // put it back in the array
+}
+
+console.log(insertionSort([1, 31, 14, 2, 3, 56, 0, -1]))
+console.log(insertionSort([-1, 0, 3, 1, 2, 14, 31, 56]))
+console.log(insertionSort([-1, 0, 1, 2, 3, 14, 31, 56]))
