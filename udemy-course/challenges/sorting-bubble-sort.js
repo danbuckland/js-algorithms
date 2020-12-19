6d06b57a9e5b462829560e8870723729744f77a2
Dan Buckland <danbucklan@gmail.com>
2020-12-19 10:58:15 +0000

    Further optimisations to bubble sort challenge

--- udemy-course/challenges/sorting-bubble-sort.js
+++ udemy-course/challenges/sorting-bubble-sort.js
@@ -3,15 +3,17 @@ const bubbleSort = (array) => {
   let swapNeeded = true
   let j = array.length
   while (swapNeeded) {
+    let swapIndex = 0
     swapNeeded = false
     for (let i = 1; i < j; i++) {
       if (array[i] < array[i - 1]) {
         swap(array, i - 1, i)
         swapNeeded = true
+        swapIndex = i
       }
       count++
     }
-    j--
+    j = swapIndex
   }
   console.log(`Took ${count} comparisons`)
   return array
@@ -25,3 +27,4 @@ const swap = (array, a, b) => {
 
 console.log(bubbleSort([1, 31, 14, 2, 3, 56, 0, -1]))
 console.log(bubbleSort([-1, 0, 3, 1, 2, 14, 31, 56]))
+console.log(bubbleSort([-1, 0, 1, 2, 3, 14, 31, 56]))
