c079da0269b771d7aa209adda56c8e935195589b
Dan Buckland <danbucklan@gmail.com>
2020-12-22 13:21:06 +0000

    Improve original rough implemention

--- udemy-course/challenges/sorting-quick-sort-rough.js
+++ udemy-course/challenges/sorting-quick-sort-rough.js
@@ -1,23 +1,23 @@
-const {performance} = require('perf_hooks');
+const { performance } = require('perf_hooks');
 // Attempt to write quickSort from the breifest of descriptions
 
-const quickSort = (array) => {
-  // grab the pivot at any index
-  // place the items either side in the array (virtually)
-
-  // Base case if the array is technically sorted
-  if (array.length <= 1) return array
-  
-  let p = 0
-  for (let i = 1; i < array.length; i++) {
-    // swap to before the greater than numbers
-    if (array[i] <= array[p]) {
-      moveItem(array, i, p)
-      p++
+const quickSort = (array, left = 0, right = array.length) => {
+  if (right > left) {
+    let p = left
+    for (let i = left + 1; i < right; i++) {
+      // swap to before the greater than numbers
+      if (array[i] <= array[left]) {
+        p++
+        moveItem(array, i, p)
+      }
     }
+    moveItem(array, left, p)
+
+    quickSort(array, left, p)
+    quickSort(array, p + 1, right)
   }
 
-  return quickSort(array.slice(0,p)).concat(array[p]).concat(quickSort(array.slice(p + 1, array.length)))
+  return array
 
 }
 
@@ -28,11 +28,13 @@ const moveItem = (array, from, to) => {
 }
 
 // Test cases:
-// console.log(quickSort([4, 3, 2, 1, 5]))
-// console.log(quickSort([1, 3, 5, 2, 1]))
-// console.log(quickSort([-1, 4, 3, 2, 5]))
+console.log(quickSort([1]))
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]))
+console.log(quickSort([4, 3, 2, 1, 5]))
+console.log(quickSort([1, 3, 5, 2, 1]))
+console.log(quickSort([-1, 4, 3, 2, 5]))
 
-let data = Array.apply(null, {length: 100000}).map(Function.call, Math.random)
+let data = Array.apply(null, {length: 10000}).map(Function.call, Math.random)
 const t1 = performance.now()
 console.log(quickSort(data))
 const t2 = performance.now()
