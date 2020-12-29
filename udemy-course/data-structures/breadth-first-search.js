5139e0b485159bb332b9b6241738b25b7f676b28
Dan Buckland <danbucklan@gmail.com>
2020-12-29 12:26:56 +0000

    Implement other DFS solutions

--- udemy-course/data-structures/breadth-first-search.js
+++ udemy-course/data-structures/breadth-first-search.js
@@ -22,8 +22,53 @@ const bfs = (tree) => {
   return visited
 }
 
-console.log(bfs(tree)) // [10, 1, 15, -2, 8, 14, 200, 13]
+const dfsPreOrder = (tree) => {
+  let visited = []
+  if (!tree.root) return visited
+  
+  const traverse = (node) => {
+    visited.push(node.value)
+    if (node.left) traverse(node.left)
+    if (node.right) traverse(node.right)
+  }
 
+  traverse(tree.root)
+  return visited
+}
+
+const dfsPostOrder = (tree) => {
+  let visited = []
+  if (!tree.root) return visited
+  
+  const traverse = (node) => {
+    if (node.left) traverse(node.left)
+    if (node.right) traverse(node.right)
+    visited.push(node.value)
+  }
+
+  traverse(tree.root)
+  return visited
+}
+
+const dfsInOrder = (tree) => {
+  let visited = []
+  if (!tree.root) return visited
+  
+  const traverse = (node) => {
+    if (node.left) traverse(node.left)
+    visited.push(node.value)
+    if (node.right) traverse(node.right)
+  }
+
+  traverse(tree.root)
+  return visited
+}
+
+
+console.log(bfs(tree))          // [10, 1, 15, -2, 8, 14, 200, 13]
+console.log(dfsPreOrder(tree))  // [10, 1, -2, 8, 15, 14, 13, 200]
+console.log(dfsPostOrder(tree)) // [-2, 8, 1, 13, 14, 200, 15, 10]
+console.log(dfsInOrder(tree))   // [-2, 1, 8, 13, 14, 200, 15, 10]
 //         10
 //       /    \
 //      1      15
