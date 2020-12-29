0036ce23e3ff0288e548f5c2323fa6fe9cb7b7c1
Dan Buckland <danbucklan@gmail.com>
2020-12-29 22:56:31 +0000

    Refactor max binary heap to make it cleaner to read

--- udemy-course/data-structures/max-binary-heap.js
+++ udemy-course/data-structures/max-binary-heap.js
@@ -19,13 +19,25 @@ class MaxBinaryHeap {
   }
 
   extractMax() {
+    if (this.values.length < 2) return this.values.pop()
+    
     this.swap(this.values.length - 1, 0)
     let popped = this.values.pop()
+    this.sink()
+    
+    return popped
+  }
+
+  sink() {
     let parent = 0
+    let value = this.values[0]
     let leftChild = parent * 2 + 1
     let rightChild = parent * 2 + 2
-    while (this.values[leftChild] && this.values[parent] < this.values[leftChild] || 
-           this.values[parent] < this.values[rightChild]) {
+    while (
+      this.values[leftChild] && 
+      value < this.values[leftChild] || 
+      value < this.values[rightChild]
+      ) {
       // swap with the higher of the two children
       if (this.values[rightChild] > this.values[leftChild]) {
         this.swap(parent, rightChild)
@@ -37,7 +49,6 @@ class MaxBinaryHeap {
       leftChild = parent * 2 + 1
       rightChild = parent * 2 + 2
     }
-    return popped
   }
 
   // Helpers
@@ -53,7 +64,9 @@ heap.insert(41).insert(39).insert(33).insert(18).insert(27).insert(12).insert(55
 console.log(heap.values)
 debugger
 heap.extractMax()
+console.log(heap.values)
 heap.extractMax()
+console.log(heap.values)
 heap.extractMax()
 console.log(heap.values)
 
