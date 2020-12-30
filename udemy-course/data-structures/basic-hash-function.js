62ad64f6a9ddd17489e38ef0a51e6ed937cb0aae
Dan Buckland <danbucklan@gmail.com>
2020-12-30 10:41:02 +0000

    Slightly improve hash function

--- udemy-course/data-structures/basic-hash-function.js
+++ udemy-course/data-structures/basic-hash-function.js
@@ -1,8 +1,9 @@
 const hash = (key, arrayLength) => {
   let total = 0
-  for (let char of key) {
-    let value = char.charCodeAt(0) - 96
-    total += value
+  const WEIRD_PRIME = 31
+  for (let i = 0; i < Math.min(100, key.length); i++) {
+    let value = key[i].charCodeAt(0) - 96
+    total = total * WEIRD_PRIME + value
   }
   return total % arrayLength
 }
