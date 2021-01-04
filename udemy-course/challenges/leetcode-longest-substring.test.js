9ecc4bfe02db124e39b019dd5bed57fc2673e3b9
Dan Buckland <danbucklan@gmail.com>
2021-01-04 10:20:11 +0000

    Fix error in test declaration

--- udemy-course/challenges/leetcode-longest-substring.test.js
+++ udemy-course/challenges/leetcode-longest-substring.test.js
@@ -1,6 +1,5 @@
-const { hasUncaughtExceptionCaptureCallback } = require('process')
 const lengthOfLongestSubstring = require('./leetcode-longest-substring')
 
 test('string of "bbbbbbb" should return 1', () => {
-  expect(lengthOfLongestSubstring("bbbbbbb")).tobe(1)
+  expect(lengthOfLongestSubstring("bbbbbbb")).toBe(1)
 })
